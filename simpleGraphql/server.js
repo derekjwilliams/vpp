5657989c79b24651af4bc64db3d45158cc1e9987
Derek Williams <derek61@gmail.com>
2023-05-04 13:40:50 -0600

    add simple graphql server and editor starting point

---
+++ simpleGraphql/server.js
@@ -0,0 +1,35 @@
+const pg = require('pg')
+const express = require('express')
+const { postgraphile } = require('postgraphile')
+const ConnectionFilterPlugin = require('postgraphile-plugin-connection-filter')
+require('dotenv').config()
+const app = express()
+
+const pgPool = new pg.Pool({
+  connectionString: (process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/covid'),
+})
+
+app.use(
+  postgraphile(
+    pgPool,
+    process.env.SCHEMA_NAMES ? process.env.SCHEMA_NAMES.split(',') : ['cim'],
+    {
+      appendPlugins: [ConnectionFilterPlugin],
+      graphileBuildOptions: {
+        connectionFilterRelations: true,
+      },
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+      dynamicJson: true,
+      enableCors: true,
+      allowExplain(req) {
+        return true
+      },
+    }
+  )
+)
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log(`🚀 Server ready at http://[host]:${port}/graphql`)
+console.log(`🚀 Graphiql UI ready at http://[host]:${port}/graphiql`)
